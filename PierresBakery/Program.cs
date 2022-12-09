944c4ec64b869f23e3fbf7900d2d002091a2d4df
Aitana Shough <aeshough@gmail.com>
2022-12-09 12:39:38 -0800

    add basic program showing bakery menu

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -9,6 +9,20 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("• • • • •");
+      Console.WriteLine("TODAY'S MENU");
+      Console.WriteLine("• • • • •");
+      Console.WriteLine("Loaf of Bread.........................$5/each");
+      Console.WriteLine("Daily Special: Buy 2 loaves, get 1 free!");
+      Console.WriteLine("• • •");
+      Console.WriteLine("Pastry................................$2/each");
+      Console.WriteLine("Daily Special: Buy 2 pastries, get 1 50% off!");
+      Console.WriteLine("• • •");
+      Console.WriteLine("Would you like to purchase any baked goods today? (Y/N)");
+      string userResponse = Console.ReadLine();
+      
+
     }
 
   }
