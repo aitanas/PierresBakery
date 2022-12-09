0d06df221652e01151f4342253bc24e858062570
Aitana Shough <aeshough@gmail.com>
2022-12-09 10:20:04 -0800

    add least amount of passing code to GetBreadPrice

--- PierresBakery/Models/Loaves.cs
+++ PierresBakery/Models/Loaves.cs
@@ -3,9 +3,15 @@ namespace Bakery.Loaves
   public class Bread
   {
     public int Quantity;
+    public int Price;
     public Bread(int quantity)
     {
       Quantity = quantity;
+    }
+
+    public int GetBreadPrice() 
+    {
+      return 10;
     }
   }
 }
