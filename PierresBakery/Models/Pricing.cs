87852a1aba18f92928c6bccd568be115ef72bb82
Aitana Shough <aeshough@gmail.com>
2022-12-09 14:49:55 -0800

    add Pricing class

---
+++ PierresBakery/Models/Pricing.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Bakery.Pricing
+{
+  public class Pricing
+  {
+    public int Price;
+    public int Quantity;
+
+    public int GetTotalPrice(int quantity, int discCost, int regCost)
+    {
+      List<int> bakeryReceipt = new List<int> {};
+      int totalPrice = 0;
+      for (int i = 1; i <= quantity; i++)
+      {
+        if (i % 3 == 0)
+        {
+          bakeryReceipt.Add(discCost);
+        }
+        else
+        {
+          bakeryReceipt.Add(regCost);
+        }
+      }
+
+      for (int j = 0; j < bakeryReceipt.Count; j++)
+      {
+        totalPrice += bakeryReceipt[j];
+      }
+      return totalPrice;
+    }
+  }
+}
