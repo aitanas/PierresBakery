13951d61dcc475dd2517de08c9c4faf6ff8c3ca7
Aitana Shough <aeshough@gmail.com>
2022-12-09 09:30:25 -0800

    update namespace names, write first bread test

---
+++ PierresBakery.Tests/ModelTests/LoavesTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Loaves;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreateInstanceOfBread_Object()
+    {
+      Bread challah = new Bread();
+      Assert.AreEqual(typeof(Bread), challah.GetType());
+    }
+  }
+}
