9ec0f9486bea0269e93ac829338d4b3bb184600d
Aitana Shough <aeshough@gmail.com>
2022-12-09 16:40:22 -0800

    WIP: implementing separation of concerns for UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -41,17 +41,13 @@ namespace Bakery
     }
       public static int BreadOrder()
       {
-        Dictionary<string, int> menu = new Dictionary<string, int>
-        {
-          {"reg", 5}, {"disc", 0}
-        };
-
-        Console.WriteLine("What kind of bread would you like today? Please select your bread using the corresponding numbers on the left.");
+        Console.WriteLine("What kind of bread would you like today? Please select using the corresponding numbers on the left.");
         Console.WriteLine("(0) No bread for me, thanks!");
         Console.WriteLine("(1) Honey Challah...................$5/loaf");
         Console.WriteLine("(2) Swiss Roll......................$5/loaf");
         Console.WriteLine("Daily Special: Buy 2 loaves, get 1 free!");
         string breadSelection = Console.ReadLine();
+
         if (int.Parse(breadSelection) > 0)
         {
           Console.WriteLine("How much bread would you like?");
@@ -67,23 +63,18 @@ namespace Bakery
 
       public static int PastryOrder()
       {
-        Dictionary<string, int> menu = new Dictionary<string, int>
-        {
-          {"reg", 2}, {"disc", 1}
-        };
-        Console.WriteLine("What kind of pastry would you like today? Please select your bread using the corresponding numbers on the left.");
+        Console.WriteLine("What kind of pastry would you like today? Please select using the corresponding numbers on the left.");
         Console.WriteLine("(0) No pastries for me, thanks!");
         Console.WriteLine("(1) Cheese Danish...................$2/each");
         Console.WriteLine("(2) Morning Bun.....................$2/each");
         Console.WriteLine("Daily Special: Buy 2 pastries, get 1 50% off!");
         string pastrySelection = Console.ReadLine();
+
         if (int.Parse(pastrySelection) > 0)
         {
           Console.WriteLine("How many pastries would you like?");
           string strPastryQuantity = Console.ReadLine();
           int pastryQuantity = int.Parse(strPastryQuantity);
-          Pastry pastryOrder = new Pastry(pastryQuantity);
-          int pastryPrice = pastryOrder.GetTotalPrice(pastryQuantity, menu["disc"], menu["reg"]);
           return pastryQuantity;
         }
         else
@@ -92,13 +83,21 @@ namespace Bakery
         }
       }
     
-      public static void OrderTotal(int breadPrice, int pastryPrice)
+      public static void OrderTotal(int breadQuantity, int pastryQuantity)
       {
-        int breadPrice = BreadOrder();
-        int pastryPrice = PastryOrder();
-          Bread breadOrder = new Bread(breadQuantity);
-          int breadPrice = breadOrder.GetTotalPrice(breadQuantity, menu["disc"], menu["reg"]);
-          Console.WriteLine("Your bread total is ${0}.", breadPrice);
+        Dictionary<string, int> menu = new Dictionary<string, int>
+        {
+          {"bread", 5}, {"discBread", 0}, {"pastry", 2}, {"discPastry", 1}
+        };
+
+        Bread breadOrder = new Bread(breadQuantity);
+        int breadPrice = breadOrder.GetTotalPrice(breadQuantity, menu["discBread"], menu["bread"]);
+        Console.WriteLine("Your bread total is ${0}.", breadPrice);
+
+        Pastry pastryOrder = new Pastry(pastryQuantity);
+        int pastryPrice = pastryOrder.GetTotalPrice(pastryQuantity, menu["discPastry"], menu["pastry"]);
+        Console.WriteLine("Your pastry total is ${0}.", pastryPrice);
+
         int subtotal = breadPrice + pastryPrice;
         Console.WriteLine("Your final total is ${0}.", subtotal);
         Console.WriteLine("Thank you for stopping by! Please come again soon!");
